2059c621971a74904e1a0ba9f8b36f54f73e9cbe
khaser <a-horohorin@mail.ru>
2021-10-05 23:43:22 +0300

    GetTests improved

--- src/test/kotlin/GetTests.kt
+++ src/test/kotlin/GetTests.kt
@@ -10,22 +10,38 @@ internal class GetTests {
     val db = saveOpenDB(fileName) ?: throw Exception("Failed create test database")
 
     @Test
-    fun singleGetInEmptyDB() {
+    fun singleGet() {
         assertEquals("hey", db.getKeyValue("testKey"))
     }
 
     @Test
+    fun singleGetWithSpaces() {
+        assertEquals("you", db.getKeyValue("  key with spaces  "))
+    }
+
+    @Test
     fun getNonExistedKey() {
-        assertEquals(db.config.defaultValue.trim(), db.getKeyValue("does not Exist"))
+        assertEquals(db.config.defaultValue, db.getKeyValue("does not Exist"))
     }
 
     @Test
     fun getLargeKey() {
-        val longKey = "longkeyxxxxxxxxxxxxxxxxxxxxxxxx"
+        val longKey = "longkey".padEnd(100)
         val stream = ByteArrayOutputStream().also { System.setOut(PrintStream(it)) }
         db.getKeyValue(longKey)
         val correctStream = ByteArrayOutputStream().also { System.setOut(PrintStream(it)) }
         println(Msg.ILLEGAL_FIELD_SIZE, "key")
+        assertEquals(correctStream.toString(), stream.toString())
+    }
+
+    @Test
+    fun getInDamagedFile() {
+        val damagedFileName = "$testDir/Damaged.db"
+        val db = saveOpenDB(damagedFileName) ?: throw Exception("Failed create test database")
+        val stream = ByteArrayOutputStream().also {System.setOut(PrintStream(it))}
+        db.getKeyValue("it should fail")
+        val correctStream = ByteArrayOutputStream().also {System.setOut(PrintStream(it))}
+        println(Msg.FILE_DAMAGED, damagedFileName)
         assertEquals(correctStream.toString(), stream.toString())
     }
 }
--- src/test/kotlin/RebuildTests.kt
+++ src/test/kotlin/RebuildTests.kt
@@ -16,25 +16,6 @@ internal class RebuildTests {
     }
 
     @Test
-    fun rebuildTest() {
-        File("$testDir/shrinkTest.db").delete()
-        File("$testDir/dumpTest.db").copyTo(File("$testDir/shrinkTest.db"))
-        var db = DB("$testDir/shrinkTest.db")
-        val options: Options = mapOf(
-            Option.PARTITIONS to "5",
-            Option.KEYS_SIZE to "100",
-            Option.VALUES_SIZE to "100",
-            Option.DEFAULT_VALUE to db.config.defaultValue.padEnd(100)
-        )
-        db = db.shrink(options)
-        val correct = File("$testDir/correct.dump").readLines().toString().let {
-            "{${it.subSequence(1, it.length - 1)}}"
-        }.toSortedSet()
-        assertEquals(correct, db.dumpAllDataBase().toString().toSortedSet())
-        File("$testDir/shrinkTest.db").delete()
-    }
-
-    @Test
     fun dumpEmptyDB() {
         File("$testDir/new.db").delete()
         val options: Options = mapOf(
@@ -42,13 +23,35 @@ internal class RebuildTests {
         )
         val db = initDataBase("$testDir/new.db", options)
         requireNotNull(db)
-        assertEquals(mapOf(), db.dumpAllDataBase())
+        assertEquals(listOf(), db.dumpAllDataBase())
         db.close()
         File("$testDir/new.db").delete()
     }
 
     @Test
-    fun rebuildEmpty() {
+    fun shrinkSimpleTest() {
+        File("$testDir/shrinkTest.db").delete()
+        File("$testDir/dumpTest.db").copyTo(File("$testDir/shrinkTest.db"))
+        var db: DB? = DB("$testDir/shrinkTest.db")
+        requireNotNull(db)
+        val options: Options = mapOf(
+            Option.PARTITIONS to "5",
+            Option.KEYS_SIZE to "100",
+            Option.VALUES_SIZE to "100",
+            Option.DEFAULT_VALUE to db.config.defaultValue.padEnd(100)
+        )
+        db = db.shrink(options)
+        requireNotNull(db)
+        val correct = File("$testDir/correct.dump").readLines().toString().let {
+            "{${it.subSequence(1, it.length - 1)}}"
+        }.toSortedSet()
+        assertEquals(correct, db.dumpAllDataBase().toString().toSortedSet())
+        File("$testDir/shrinkTest.db").delete()
+    }
+
+
+    @Test
+    fun shrinkEmpty() {
         File("$testDir/new.db").delete()
         var db = initDataBase("$testDir/new.db", Config())
         requireNotNull(db)
@@ -57,8 +60,26 @@ internal class RebuildTests {
             Option.KEYS_SIZE to "22"
         )
         db = db.shrink(options)
-        assertEquals(mapOf(), db.dumpAllDataBase())
+        requireNotNull(db)
+        assertEquals(listOf(), db.dumpAllDataBase())
         File("$testDir/new.db").delete()
     }
 
+    @Test
+    fun shrinkOldDefaultTooLargeNoNewDefault() {}
+
+    @Test
+    fun shrinkOldDefaultTooLargeButNewDefaultIsGood() {}
+
+    @Test
+    fun shrinkOldDefaultAndNewDefaultTooLarge() {}
+
+    @Test
+    fun shrinkOldDefaultIsGoodButNewDefaultTooLarge() {}
+
+    @Test
+    fun shrinkAllEntriesTooLarge() { }
+
+    @Test
+    fun shrinkSomeEntriesTooLarge() { }
 }
